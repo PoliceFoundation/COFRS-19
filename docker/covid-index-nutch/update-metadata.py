bd81acd3ad903724f2ea09a304104c0332a03faf
Scott Came <scottcame10@gmail.com>
2020-04-07 19:41:57 -0700

    Accommodate new metadata sheet schema

--- docker/covid-index-nutch/update-metadata.py
+++ docker/covid-index-nutch/update-metadata.py
@@ -1,31 +1,36 @@
 import pandas as pd
 import json
 import requests
+import re
 
 df = pd.read_excel('/tmp/nutch/Solr Schema COVID-19 PF.xlsx')
+
+hashStrip = re.compile("(.+)#.+")
+multispace = re.compile("[ ]+")
 
 payload = []
 
 for index, row in df.iterrows():
+
+  url = row['URL']
+
+  if re.match("^http", url):
+
+    url = hashStrip.sub("\\1", url)
   
-  purpose = []
-  purposeField = row['Content Purpose']
-  if isinstance(purposeField, str) and not purposeField is None:
-    purpose = purposeField.split(",")
+    print("Updating metadata for URL (id): " + url)
 
-  tags = []
-  tagsField = row['Tags']
-  if isinstance(tagsField, str) and not tagsField is None:
-    tags = tagsField.split(",")
+    purpose = row['Content Purpose']
 
-  payload.append({
-    "id": row['URL'],
-    "covid_owner": {"set": row['Owner']},
-    "covid_description": {"set": row['Description']},
-    "covid_purpose": {"set": purpose},
-    "covid_tags": {"set": tags}
-  })
+    tags = [row['Tag 1'], row['Tag 2'], row['Tag 3'], row['Tag 4'], row['Tag 5']]
+    tags = [t for t in tags if t and isinstance(t, str) and t != '' and t != 'null' and t != 'NaN']
 
-url = "http://solr:8983/solr/covid/update?commitWithin=1000"
-requests.post(url, json=payload)
+    payload.append({
+      "id": url,
+      "covid_owner": {"set": row['Owner']},
+      "covid_description": {"set": row['Title']},
+      "covid_purpose": {"set": purpose},
+      "covid_tags": {"set": tags}
+    })
 
+requests.post("http://solr:8983/solr/covid/update?commitWithin=1000", json=payload)
--- docker/covid-index-nutch/write-seeds.py
+++ docker/covid-index-nutch/write-seeds.py
@@ -1,6 +1,15 @@
 import pandas as pd
+import re
 
 df = pd.read_excel('/tmp/nutch/Solr Schema COVID-19 PF.xlsx')
 
+files = 0
+
 with open("/tmp/seeds.txt", "w") as outfile:
-  outfile.write("\n".join(df['URL'].values.tolist()))
+  for index, row in df.iterrows():
+    url = row['URL']
+    if re.match("^http", url):
+      outfile.write(url + "\n")
+      files = files + 1
+
+print("Wrote " + str(files) + " seed URLs for nutch processing")
